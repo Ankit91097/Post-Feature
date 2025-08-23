e8c2d91e114ee5006df171d05e97c0a8b8105f09
Ankit Niranjan <ankitniranjan331@gmail.com>
2025-08-23 13:23:37 +0530

    server running on one port

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,14 @@ const multer = require("multer");
 const uploadFile = require("./services/Post.services");
 const postModel = require("./models/post.model");
 const cors = require("cors");
+const path = require("path");
 const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("public"));
 
-app.post("/posts", upload.single("file"), async (req, res) => {
+app.post("/api/posts", upload.single("file"), async (req, res) => {
   try {
     const file = req.file.buffer;
     const result = await uploadFile(file, "file");
@@ -26,7 +28,7 @@ app.post("/posts", upload.single("file"), async (req, res) => {
   }
 });
 
-app.get("/posts", async (req, res) => {
+app.get("/api/posts", async (req, res) => {
   try {
     const post = await postModel.find();
     res.json({
@@ -36,6 +38,10 @@ app.get("/posts", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+});
+
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = app;
